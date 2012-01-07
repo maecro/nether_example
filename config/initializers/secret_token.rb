e67c9854844878df08aa3cf8f2d30dfad87812d5
Matthew Cross <matthew.evan.cross@gmail.com>
2012-01-08 03:06:34 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetherExamlpe::Application.config.secret_token = 'dfb08bfa04fac114da41215be40a94636e5f088de3c4d441fcc8c954afdc47a71957158838a1ccb0843c8c268456787494de6539107b971e5672dca5ac85d459'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  require 'populator'
+  
+  [Post].each(&:delete_all)
+  
+  Post.populate 20 do |post|
+    post.title = Populator.words(1..3).titleize
+    post.body = Populator.sentences(2..10)
+  end
